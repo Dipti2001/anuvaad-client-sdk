34a9b0e1930af585a5711e908062add3e13deffd
Dipti Mondal <76087020+Dipti2001@users.noreply.github.com>
2024-05-22 13:44:33 +0530

    Update logs asr.js

--- src/asr.js
+++ src/asr.js
@@ -27,20 +27,27 @@ const api = new BaseApi;
  * console.log('Recognized Text:', recognizedText);
  */
 async function asr(sourceLang, base64) {
-
-    const payload = await asr_payload(sourceLang, base64)
+    const payload = await asr_payload(sourceLang, base64);
+    console.log("Payload:", payload);
 
     try {
-        const res = await api.post(payload)
-        return res.data.pipelineResponse[0].output[0].source;
+        const res = await api.post(payload);
+        console.log("API response data:", res.data);
 
+        if (res.data.pipelineResponse && res.data.pipelineResponse[0] && res.data.pipelineResponse[0].output[0]) {
+            return res.data.pipelineResponse[0].output[0].source;
+        } else {
+            throw new Error('Invalid response structure');
+        }
     } catch (error) {
-        console.log('error in response', error)
+        console.log('Error in response:', error);
+        if (error.response) {
+            console.error("Bhashini API response error:", error.response.data);
+        }
+        throw error;
     }
-
 }
 
-
-export default asr
+export default asr;
 
 
